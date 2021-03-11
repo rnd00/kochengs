6f29c1112b0ea5829eb080250c2d3c0ab8e6ada2
r-diedan <r-diedan@almex.jp>
2021-03-11 15:34:46 +0900

    kochengs working

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rnd00/kochengs
 
 go 1.15
+
+require github.com/rs/zerolog v1.20.0
--- kochengs.go
+++ kochengs.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"flag"
+
+	"github.com/rnd00/kochengs/pull"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	amt := flag.Int("amt", 1, "Amount of cats will to generate")
+	flag.Parse()
+
+	log.Info().Msg("Kocheng started!")
+	log.Info().Msgf("Will run for %d times!", *amt)
+	for i := 0; i < *amt; i++ {
+		pull.ThisCatDoesNotExist()
+	}
+}
